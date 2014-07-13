opencog/opencog

commit 49aec20db4bc2f5c9ac0b3fcd3e812e06e662581

Changed r2l negative helper function to create PLN suitable output

opencog/nlp/scm/relex-to-logic.scm
@@ -135,9 +135,8 @@
 	)
 )
 
-(define (negative-rule verb instance) 
-	(list (ImplicationLink (PredicateNode instance df-node-stv) (PredicateNode verb df-node-stv) df-link-stv)
-	(NotLink (PredicateNode instance df-node-stv) df-link-stv)
+(define (negative-rule verb instance)
+	(ImplicationLink (PredicateNode instance df-node-stv) (NotLink (PredicateNode verb df-node-stv) df-link-stv) df-link-stv)
 	)
 )
 

opencog/ure

commit bb81e659805d4af07ccff561a0b21b6e5478ffd1

Fix regression introduced by commit https://github.com/opencog/opencog/commit/49aec20db4bc2f5c9ac0b3fcd3e812e06e662581#diff-0141897e92401b447418eef495135e7eL142
```
Warning file /home/ceefour/git/opencog/opencog/nlp/scm/relex-to-logic.scm ended with unterminated input begun at line 140
scm_unprotect_object called on unprotected object
[2014-07-13 06:18:09:507] [ERROR] Caught signal 6 (Aborted) on thread 140628137334784
        Stack Trace:
        2: opencog/server/cogserver (opencog::Logger::Base::~Base() +0x7c) [0x41743e]
```

opencog/nlp/scm/relex-to-logic.scm
@@ -137,7 +137,6 @@
 
 (define (negative-rule verb instance)
 	(ImplicationLink (PredicateNode instance df-node-stv) (NotLink (PredicateNode verb df-node-stv) df-link-stv) df-link-stv)
-	)
 )
 
 (define (possessive-rule noun noun_instance word word_instance)