dd41ca5c53940298659cd0fccd8cca434e1c5424
Nil Geisweiller <ngeiswei@gmail.com>
2019-11-15 08:21:13 +0200

    Use gen_variables instead of gen_varlist

--- opencog/unify/Unify.cc
+++ opencog/unify/Unify.cc
@@ -221,8 +221,8 @@ void Unify::set_variables(const Handle& lhs, const Handle& rhs,
                           const Handle& lhs_vardecl, const Handle& rhs_vardecl)
 {
 	// Merge the 2 type declarations
-	Variables lv = gen_varlist(lhs, lhs_vardecl)->get_variables();
-	Variables rv = gen_varlist(rhs, rhs_vardecl)->get_variables();
+	Variables lv = gen_variables(lhs, lhs_vardecl);
+	Variables rv = gen_variables(rhs, rhs_vardecl);
 	_variables = merge_variables(lv, rv);
 }
 
