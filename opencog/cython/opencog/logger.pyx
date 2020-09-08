4083d2354302ba985093d2cdfd54e50176862a07
Nil Geisweiller <ngeiswei@gmail.com>
2020-09-08 15:46:35 +0300

    Pass c_ure_logger() output directly by reference

--- opencog/cython/opencog/logger.pyx
+++ opencog/cython/opencog/logger.pyx
@@ -2,6 +2,5 @@ from ure cimport ure_logger as c_ure_logger
 from opencog.logger cimport wrap_clogger, cLogger
 
 def ure_logger():
-    cdef cLogger cl = c_ure_logger()
-    z= wrap_clogger(&cl)
+    z = wrap_clogger(&c_ure_logger())
     return z
