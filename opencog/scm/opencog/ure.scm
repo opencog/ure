0ede7b20866176e30bab7b332637fd359f97a3bc
Linas Vepštas <linasvepstas@gmail.com>
2024-04-02 00:48:20 -0500

    Move URE types from atomspace to URE

--- opencog/scm/opencog/ure.scm
+++ opencog/scm/opencog/ure.scm
@@ -1,11 +1,19 @@
 ;
 ; OpenCog Unified Rule Engine module
 ;
-(define-module (opencog ure))
+(define-module (opencog ure)
+ #:use-module (opencog)
+ #:use-module (opencog ure-config)
+)
 
-(use-modules (opencog ure-config))
-(use-modules (opencog logger))
-(load-extension (string-append opencog-ext-path-ure "libure") "opencog_ure_init")
+(load-extension
+   (string-append opencog-ext-path-ure-types "libure-types")
+   "ure_types_init")
 
+(load-extension
+	(string-append opencog-ext-path-ure "libure")
+	"opencog_ure_init")
+
+(include-from-path "opencog/ure/types/ure_types.scm")
 (load-from-path "opencog/ure/ure-utils.scm")
 (export-ure-utils)
